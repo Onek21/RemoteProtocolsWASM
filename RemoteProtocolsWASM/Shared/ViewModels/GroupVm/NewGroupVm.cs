40677ac8596a60a15aef7d2ea7e048182b5afa79
Szymon Kląskała <klaskala.sz@gmail.com>
2021-09-21 08:35:54 +0200

    feat: craete group viewmodels

---
+++ RemoteProtocolsWASM/Shared/ViewModels/GroupVm/NewGroupVm.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using RemoteProtocolsWASM.Domain.Model;
+using RemoteProtocolsWASM.Shared.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteProtocolsWASM.Shared.ViewModels.GroupVm
+{
+    public class NewGroupVm : IMapFrom<Group>
+    {
+        public int GroupId { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<NewGroupVm, Group>().ReverseMap();
+        }
+    }
+}
